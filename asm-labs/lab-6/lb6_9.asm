42e6b485e8ba9a5eb78fa6005162d709a3594d52
Chebupellich <chebupellich@gmail.com>
2024-10-16 21:50:32 +0400

    asm: upd

--- asm-labs/lab-6/lb6_9.asm
+++ asm-labs/lab-6/lb6_9.asm
@@ -4,19 +4,19 @@ input_value:            ; A123F1C0 hex
 
 condition_1:            ; b8 < 0 && b9 < 0
     cmplt b8, 0, b10
-    cmplt b9, 0, b11
-    and b10, b11, b11
+    cmplt b9, 0, b12
+    and b10, b11, b1
 
-    [b11] b operation_1
-    [!b11] b condition_2
+    [b1] b operation_1
+    [!b1] b condition_2
     nop 5
 
 condition_2:            ; b8 ^ b9 -> res > 0
     xor b8, b9, b10
-    cmpgt b10, 0, b11
+    cmpgt b10, 0, b1
 
-    [b11] b operation_2
-    [!b11] b operation_3
+    [b1] b operation_2
+    [!b1] b operation_3
 
     nop 5
 
@@ -31,7 +31,7 @@ operation_2:            ; put '1' in range 0-14 of source val
     nop 5
 
 operation_3:            ; move val on other side
-    mv A9 B9
+    mv A9, B9
 
 end:
     nop 1
